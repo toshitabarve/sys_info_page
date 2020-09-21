facd7fe6f367560fd49e65637ab706c715db504c
Toshita Barve <tbarve@login2.ufhpc>
2020-09-20 22:54:47 -0400

    Added functions

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -8,14 +8,17 @@ CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
 report_uptime () {
+  echo "Function report_uptime executed."
   return
 }
 
 report_disk_space () {
+  echo "Function report_disk_space executed."
   return
 }
 
 report_home_space () {
+  echo "Function report_home_space executed."
   return
 }
 
